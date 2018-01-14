482ca4c1470026c1dd015fb0fcb8137325454447
Craig Curtis <craigcurtis@Craigs-MacBook-Pro.local>
2018-01-14 15:45:43 -0700

    Init commit, add Sass

---
+++ src/components/Forum.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Forum.css';
+
+class Forum extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default Forum;
