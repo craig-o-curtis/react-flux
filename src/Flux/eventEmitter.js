f74a5e6504d03fe1dc4dddc25e3e1e6471e5bb1b
Craig Curtis <craigcurtis@Craigs-MacBook-Pro.local>
2018-04-05 21:13:34 -0700

    Add npm events package for more robust event emitter

--- src/Flux/eventEmitter.js
+++ src/Flux/eventEmitter.js
@@ -1,34 +1,34 @@
 
 
+// sample less-robust event emitter:
+// class EventEmitter {
 
-class EventEmitter {
+//     constructor() {
+//         this._events = {}
+//     }
+//     // methods
 
-    constructor() {
-        this._events = {}
-    }
-    // methods
+//     // pushes functions into an array, called by emit
+//     on = (type, listener) => {
+//         this._events[type] = this._events[type] || [];
+//         this._events[type].push(listener);
+//     }
 
-    // pushes functions into an array, called by emit
-    on = (type, listener) => {
-        this._events[type] = this._events[type] || [];
-        this._events[type].push(listener);
-    }
+//     // locates that type array on _events object, calls each item/fn in array
+//     emit = (type) => {
+//          if (this._events[type]) {
+//              this._events[type].forEach( (listener) => {
+//                 listener();
+//              });
+//          }
+//     }
 
-    // locates that type array on _events object, calls each item/fn in array
-    emit = (type) => {
-         if (this._events[type]) {
-             this._events[type].forEach( (listener) => {
-                listener();
-             });
-         }
-    }
+//     removeListener = (type, listener) => {
+//         if (this._events[type]) {
+//             this._events[type].splice(this._events[type].indexOf(listener),1); 
+//         }
+//     }
 
-    removeListener = (type, listener) => {
-        if (this._events[type]) {
-            this._events[type].splice(this._events[type].indexOf(listener),1); 
-        }
-    }
+// }
 
-}
-
-export default EventEmitter;
+// export default EventEmitter;
--- src/Stores/ForumStore.js
+++ src/Stores/ForumStore.js
@@ -1,4 +1,5 @@
-import EventEmitter from '../Flux/eventEmitter';
+// import EventEmitter from '../Flux/eventEmitter'; // custom
+import EventEmitter from 'events';
 import ForumDispatcher from '../Flux/ForumDispatcher';
 import ForumConstants from '../constants/ForumConstants';
 // const answerData = {
